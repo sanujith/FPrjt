d790a33766de3a3639c125fe5ef48d6c99e44e74
Suren Baghdasaryan <surenb@google.com>
2023-06-16 18:22:16 +0000

    RMX2061: Disable per-cgroup PSI accounting
    Per-cgroup PSI accounting is not used in Android while having small
    performance cost. Disable this unused feature.
    
    Bug: 233410456
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Ib5174d7d819e33e2fc2f71bfafb68e8f4b79631a
    Merged-In: Ib5174d7d819e33e2fc2f71bfafb68e8f4b79631a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: GenZSouL <priyanshusinghal0818ext@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,6 +130,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += kpti=off
+BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
 
 # Media
 TARGET_USES_ION := true
