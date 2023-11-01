b3271afb32dfc507699fb5e8d7bff0379dc33b52
Egor Pilipenko <pilipenko0409@gmail.com>
2023-11-01 11:10:50 +0700

    RMX2061: Cleanup removable GAPPS list -Add back prebuilt file manager

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,12 +4,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService Camera2 ConnMO DCMO
-LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES := AndroidAutoStubPrebuilt AmbientSensePrebuilt AppDirectedSMSService Camera2 ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService GCS MaestroPrebuilt MicropaperPrebuilt MyVerizonServices
 LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Ornament SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
 LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger WallpapersBReel2020 obdm_stub DevicePolicyPrebuilt RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += Camera2 YouTube FilesPrebuilt AudioFX Recorder
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger obdm_stub DevicePolicyPrebuilt RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += YouTube AudioFX Recorder Videos bcr
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
