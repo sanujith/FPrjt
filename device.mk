6aa45e2f471ec70b9c18d4f7e673aa2d97c61410
Egor Pilipenko <pilipenko0409@gmail.com>
2023-11-10 21:18:48 +0700

    RMX2061: Build vendor.qti.hardware.bluetooth_audio@2.0 from source

--- device.mk
+++ device.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     android.hardware.bluetooth.a2dp@1.0 \
